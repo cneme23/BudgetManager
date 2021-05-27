12f630ff78b5d81951215f107f6eaa254209972f
carlos neme <carlosneme23@gmail.com>
2021-05-27 02:01:00 -0300

    adding changes

--- budget-manager/src/components/Question.js
+++ budget-manager/src/components/Question.js
@@ -1,15 +1,51 @@
 import React, {useState} from "react";
+import Error from "./Error";
 
 const Question = () => {
+    
+    //Aca definimos el primer State. Inicialmente va a estar en 0
+    const [quantity,saveQuantity] = useState(0);
+
+    //Aca definimos el segundo State para manejar las validaciones
+    const [error,saveError]= useState(false);
+    
+    //Aca definimos la funcion que se encarga de leer el presupuesto
+    //Esta funcion a su vez se encarga de ejecutar la funcion "saveQuantity " que es la encargada de modificar el estado del componente
+    //Con el metodo parseÍnt , me aseguro de convertir a entero todo
+    const readBudget= e => {
+        saveQuantity( parseInt(e.target.value,10) )
+    }
+    
+    //Submit para agregar el presupuesto
+
+    const addingBudget = e => {
+        e.preventDefault();
+    //Validación: Aqui validamos que el usuario no deje vacio el input o ponga valores negativos
+
+    if(quantity <1 || isNaN(quantity) ){
+        saveError(true);
+        return; 
+    }
+
+    //Si pasa la validacion
+    saveError(false);
+    
+    }
+    
+
+
     return(
         <>
         <h2>Insert your budget</h2>
+        {error ? <Error message="The budget is incorrect" /> : null}
         
-        <form>
+        <form
+            onSubmit={addingBudget}>
             <input
                 type="number"
                 className="u-full-width"
                 placeholder="Insert your budget"
+                onChange={readBudget}
             />
             <input
                 type="submit"
